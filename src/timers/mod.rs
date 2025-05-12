55775c212d356ef4c3f3c80cba5a89784dfaea09
Joshua Griffith <Joshua.Griffith@fnf.com>
2025-05-12 15:42:02 -0500

    fix: active should include scheduled

--- src/timers/mod.rs
+++ src/timers/mod.rs
@@ -3,27 +3,89 @@
 use crate::Key;
 use ahash::{HashMap, HashMapExt, HashSet};
 use chrono::{DateTime, OutOfRangeError, Utc};
+use futures::TryFutureExt;
 use scc::hash_map::Entry;
 use std::future::poll_fn;
 use std::sync::Arc;
 use thiserror::Error;
-use tokio::select;
 use tokio::sync::mpsc::error::TrySendError;
 use tokio::sync::{mpsc, oneshot};
+use tokio::{select, spawn};
 use tokio_util::time::{DelayQueue, delay_queue};
 use tracing::error;
+
+const BUFFER_SIZE: usize = 64;
 
 type ActiveTriggers = scc::HashMap<Key, HashSet<DateTime<Utc>>>;
 
 #[derive(Clone, Debug)]
 struct Timer {
-    commands: mpsc::Sender<Command>,
+    command_tx: mpsc::Sender<Command>,
     active_triggers: Arc<ActiveTriggers>,
+}
+
+impl Timer {
+    pub fn new() -> (mpsc::Receiver<Trigger>, Self) {
+        let (command_tx, commands_rx) = mpsc::channel(BUFFER_SIZE);
+        let (triggers_tx, triggers_rx) = mpsc::channel(BUFFER_SIZE);
+        let active_triggers = Arc::new(ActiveTriggers::new());
+
+        spawn(process_commands(
+            commands_rx,
+            triggers_tx,
+            active_triggers.clone(),
+        ));
+
+        (
+            triggers_rx,
+            Self {
+                command_tx,
+                active_triggers,
+            },
+        )
+    }
+
+    pub async fn schedule(&self, key: Key, time: DateTime<Utc>) -> Result<(), TimerError> {
+        let (result_tx, result_rx) = oneshot::channel();
+        self.command_tx
+            .send(Command {
+                result_tx,
+                key,
+                time,
+                operation: CommandOperation::Remove,
+            })
+            .map_err(|_| TimerError::Shutdown)
+            .await?;
+
+        result_rx.map_err(|_| TimerError::Shutdown).await?
+    }
+
+    pub async fn unschedule(&self, key: Key, time: DateTime<Utc>) -> Result<(), TimerError> {
+        let (result_tx, result_rx) = oneshot::channel();
+        self.command_tx
+            .send(Command {
+                result_tx,
+                key,
+                time,
+                operation: CommandOperation::Remove,
+            })
+            .map_err(|_| TimerError::Shutdown)
+            .await?;
+
+        result_rx.map_err(|_| TimerError::Shutdown).await?
+    }
+
+    pub async fn active_times(&self, key: &Key) -> Option<Vec<DateTime<Utc>>> {
+        let times = self.active_triggers.get_async(key).await?;
+        let mut sorted_times: Vec<DateTime<Utc>> = times.iter().copied().collect();
+        sorted_times.sort();
+        Some(sorted_times)
+    }
 }
 
 #[derive(Debug)]
 struct Command {
-    result: oneshot::Sender<Result<(), TimerError>>,
+    result_tx: oneshot::Sender<Result<(), TimerError>>,
     key: Key,
     time: DateTime<Utc>,
     operation: CommandOperation,
@@ -39,36 +101,29 @@ enum CommandOperation {
 struct Trigger {
     key: Key,
     time: DateTime<Utc>,
-    operation: TriggerOperation,
-}
-
-#[derive(Copy, Clone, Debug)]
-enum TriggerOperation {
-    Schedule,
-    Remove,
 }
 
 async fn process_commands(
     mut commands: mpsc::Receiver<Command>,
     triggers: mpsc::Sender<Trigger>,
-    active: &ActiveTriggers,
+    active: Arc<ActiveTriggers>,
 ) {
     let mut queue = DelayQueue::new();
     let mut queue_keys = HashMap::new();
-    let mut maybe_trigger: Option<Trigger> = None;
+    let mut trigger_to_send: Option<Trigger> = None;
 
     loop {
-        if let Some(trigger) = &maybe_trigger {
+        if let Some(trigger) = &trigger_to_send {
             select! {
                 result = commands.recv() => {
                     let Some(command) = result else {
                         break;
                     };
-                    process_command(&mut queue, &mut queue_keys, active, command).await;
+                    process_command(&mut queue, &mut queue_keys, &active, command).await;
                 }
 
                 result = triggers.send(trigger.clone()) => {
-                    maybe_trigger.take();
+                    trigger_to_send.take();
                     if result.is_err() {
                         break;
                     }
@@ -80,19 +135,17 @@ async fn process_commands(
                     let Some(command) = result else {
                         break;
                     };
-                    process_command(&mut queue, &mut queue_keys, active, command).await;
+                    process_command(&mut queue, &mut queue_keys, &active, command).await;
                 }
 
                 Some(expired) = poll_fn(|cx| queue.poll_expired(cx)) => {
                     let (key, time) = expired.into_inner();
-                    active.entry_async(key.clone()).await.or_default().get_mut().insert(time);
                     if let Err(error) = triggers.try_send(Trigger {
                         key,
                         time,
-                        operation: TriggerOperation::Remove,
                     }) {
                         match error {
-                            TrySendError::Full(trigger) => maybe_trigger = Some(trigger),
+                            TrySendError::Full(trigger) => trigger_to_send = Some(trigger),
                             TrySendError::Closed(_) => break
                         }
                     }
@@ -109,14 +162,15 @@ async fn process_command(
     command: Command,
 ) {
     match command.operation {
-        CommandOperation::Add => add_time(queue, queue_keys, command),
+        CommandOperation::Add => add_time(queue, queue_keys, active, command).await,
         CommandOperation::Remove => remove_time(queue, queue_keys, active, command).await,
     }
 }
 
-fn add_time(
+async fn add_time(
     queue: &mut DelayQueue<(Key, DateTime<Utc>)>,
     queue_keys: &mut HashMap<(Key, DateTime<Utc>), delay_queue::Key>,
+    active: &ActiveTriggers,
     command: Command,
 ) {
     let time = command.time;
@@ -125,14 +179,22 @@ fn add_time(
     let duration = match time.signed_duration_since(Utc::now()).to_std() {
         Ok(duration) => duration,
         Err(error) => {
-            send_result(command.result, &key, &time, Err(error.into()));
+            send_result(command.result_tx, &key, &time, Err(error.into()));
             return;
         }
     };
 
     let queue_key = queue.insert((key.clone(), time), duration);
     queue_keys.insert((key.clone(), time), queue_key);
-    send_result(command.result, &key, &time, Ok(()));
+
+    active
+        .entry_async(key.clone())
+        .await
+        .or_default()
+        .get_mut()
+        .insert(time);
+
+    send_result(command.result_tx, &key, &time, Ok(()));
 }
 
 async fn remove_time(
@@ -142,7 +204,10 @@ async fn remove_time(
     command: Command,
 ) {
     let Command {
-        result, key, time, ..
+        result_tx: result,
+        key,
+        time,
+        ..
     } = command;
 
     let scheduled_entry = (key, time);
@@ -189,4 +254,7 @@ pub enum TimerError {
 
     #[error("Time not found")]
     NotFound,
+
+    #[error("Timer has been shutdown")]
+    Shutdown,
 }
