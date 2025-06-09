07da389dfe924be59045b33793ae527984832e6b
Joshua Griffith <Joshua.Griffith@fnf.com>
2025-06-09 11:46:56 -0500

    fix: test

--- src/timers/manager.rs
+++ src/timers/manager.rs
@@ -994,6 +994,7 @@ mod tests {
     }
 
     #[tokio::test]
+    #[allow(clippy::unwrap_used)]
     async fn test_concurrent_operations() -> Result<()> {
         time::pause();
 
@@ -1007,7 +1008,7 @@ mod tests {
         for i in 0..10 {
             let manager_clone = manager.clone();
             let handle = spawn(async move {
-                let trigger = create_test_trigger(&format!("concurrent-{}", i), 60 + i)?;
+                let trigger = create_test_trigger(&format!("concurrent-{}", i), 60 + i).unwrap();
                 manager_clone.schedule(trigger).await
             });
             handles.push(handle);
